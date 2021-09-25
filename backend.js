9bac21cbe62e6aff76e4b42bba790d9a7dc841b0
jaron-cui <jaron.cui@gmail.com>
2021-09-25 14:45:48 -0400

    trying sleep

--- backend.js
+++ backend.js
@@ -1,1 +1,11 @@
-alert("What's up?")
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+
+for(n = 0; n < 100; n += 1) {
+    await sleep(1000)
+    document.getElementById("display").innerHTML = toString(n)
+}

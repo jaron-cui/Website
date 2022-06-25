b78971272552098691234de4dd37dbdbe7e14bbb
jaron-cui <jaron.cui@gmail.com>
2022-06-25 14:04:05 -0400

    rebuilding app in ts react

--- navbar.js
+++
@@ -1,9 +0,0 @@
-document.appendChild(
-
-        <ul>
-            <li><a class="active" href="index.html">Home</a></li>
-            <li><a href="projects.html">Projects</a></li>
-            <li><a href="google.com">Resume</a></li>
-        </ul>
-
-)

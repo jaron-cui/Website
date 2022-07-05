8132a59e00b688d6049c961bb0948d287eb90125
jaron-cui <jaron.cui@gmail.com>
2022-07-05 15:46:01 -0400

    created placeholder experience page

---
+++ src/Experience.tsx
@@ -0,0 +1,8 @@
+import { DEFAULT_FONT } from "./constants";
+import NotFound from "./NotFound";
+
+export default function Experience() {
+  return (
+    <NotFound message='I have not yet created this experience page. Coming soon!'/>
+  );
+}

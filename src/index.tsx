bd3bc7709b1749863e304888504a1eacc051c97f
jaron-cui <jaron.cui@gmail.com>
2023-03-16 23:02:57 -0400

    no-cache?

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+    <meta http-equiv='cache-control' content='no-cache'/> 
+    <meta http-equiv='expires' content='0'/> 
+    <meta http-equiv='pragma' content='no-cache'></meta>
     <BrowserRouter>
       <App />
     </BrowserRouter>

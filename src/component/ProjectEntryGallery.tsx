20a5e8222283fefe691ea65ac9a0414280e70919
jaron-cui <jaron.cui@gmail.com>
2023-12-11 16:22:51 -0500

    updated projects

--- src/component/ProjectEntryGallery.tsx
+++ src/component/ProjectEntryGallery.tsx
@@ -1,24 +1,28 @@
 import { Tooltip } from "@mui/material";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Media } from "../util/types";
 import { toDownloadLink, mod } from "../util/util";
 import { SideButton } from "./Buttons";
 
 export default function ProjectEntryGallery({ media } : { media: Media[] }) {
   const [index, setIndex] = useState(0);
-  const cachedMedia = useRef<JSX.Element[]>(
-    media.map(item => <img src={toDownloadLink(item.link)} style={{objectFit: 'contain', width: '100%'}}/>
-  ));
+  const cachedMedia = useRef<JSX.Element[]>([]);
 
   const changeImage = (increment: number) => () => {
     const newIndex = mod(index + increment, media.length); 
     setIndex(newIndex);
   };
 
+  useEffect(() => {
+    cachedMedia.current = media.map(
+      item => <img src={toDownloadLink(item.link)} style={{objectFit: 'contain', width: '100%'}}/>
+    );
+  }, []);
+
   return (
     <div style={{height: '300px', width: '100%', display: 'flex', justifyContent: 'center'}}>
       <SideButton direction='left' onClick={changeImage(-1)}/>
-      <Tooltip title={media[index].description} leaveDelay={4000}>
+      <Tooltip title={media[index].description} leaveDelay={0}>
         <div style={{display: 'flex', backgroundColor: '#999999', height: '100%', width: 'max(50%, 700px)'}}>{cachedMedia.current[index]}</div>
       </Tooltip>
       <SideButton direction='right' onClick={changeImage(1)}/>

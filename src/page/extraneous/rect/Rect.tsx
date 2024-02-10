b0415c63f1c19ccb6ce865264a9bed8bb1bc88e2
jaroncui <jaron.cui@gmail.com>
2024-02-09 22:00:32 -0500

    created parkour course

--- src/page/extraneous/rect/Rect.tsx
+++ src/page/extraneous/rect/Rect.tsx
@@ -8,6 +8,28 @@ import { Player } from './entity/player';
 import { Dynamite } from "./entity/dynamite";
 import { DEFAULT_INPUT_MAP, InputHandler } from './input';
 
+const template = [
+  '            x                               ',
+  '            x                   t           ',
+  '            x                   g             ',
+  '        t   x                   bbbbbbx       ',
+  '   g   ggg  x                    b            ',
+  '   x     d  x            x       b     t tt   ',
+  '   xx    d  x         x        x b xbbxggxggxx',
+  '         b  x                  x b xsx        ',
+  '         b                     x b ssx        ',
+  ' xbbb    b     t   g           x b xxx        ',
+  '  xxd       x xg   s           x b xxx        ',
+  '  xdx              xt          x b xxx        ',
+  's xdx               g          x b xdd        ',
+  'x  xx               d          x b xbd        ',
+  'x  xx               x          x b xbb        ',
+  'xd xx                x         x b bbx        ',
+  'xd                   d        tx   bbb        ',
+  'xx    tt  t     ttt  d    t  ggx   bbb        ',
+  'xxssxgggddsgxdxggxsdxdssxxgssssxxxxxxxxxxxxxx'
+]
+
 const WORLD = new Terrain(WORLD_WIDTH, WORLD_HEIGHT);
 for (let x = 0; x < WORLD_WIDTH; x += 1) {
   for (let y = 0; y < WORLD_HEIGHT; y += 1) {
@@ -24,17 +46,34 @@ for (let x = 0; x < WORLD_WIDTH; x += 1) {
     }
   }
 }
-WORLD.set(17, 16, Block.Soil);
-WORLD.set(22, 17, Block.Soil);
-WORLD.set(22, 18, Block.Soil);
-WORLD.set(22, 19, Block.Soil);
-WORLD.set(22, 20, Block.Soil);
 
-WORLD.set(19, 18, Block.Soil);
-WORLD.set(19, 19, Block.Soil);
-WORLD.set(19, 20, Block.Soil);
-WORLD.set(19, 21, Block.Soil);
-WORLD.set(19, 22, Block.Soil);
+const blocks: Record<string, Block> = {
+  ' ': Block.Air,
+  'x': Block.Stone,
+  'd': Block.Soil,
+  'g': Block.Grass,
+  't': Block.Grasses,
+  's': Block.Sand,
+  'b': Block.Brick
+}
+
+for (let y = 0; y < template.length; y += 1) {
+  for (let x = 0; x < template[y].length; x += 1) {
+    const block = blocks[template[template.length - y - 1][x]];
+    WORLD.set(x + 5, y + 6, block);
+  }
+}
+// WORLD.set(17, 16, Block.Soil);
+// WORLD.set(22, 17, Block.Soil);
+// WORLD.set(22, 18, Block.Soil);
+// WORLD.set(22, 19, Block.Soil);
+// WORLD.set(22, 20, Block.Soil);
+
+// WORLD.set(19, 18, Block.Soil);
+// WORLD.set(19, 19, Block.Soil);
+// WORLD.set(19, 20, Block.Soil);
+// WORLD.set(19, 21, Block.Soil);
+// WORLD.set(19, 22, Block.Soil);
 const worldData = PIXI.BaseTexture.fromBuffer(WORLD.blocks, WORLD.w, WORLD.h, { format: PIXI.FORMATS.ALPHA, type: PIXI.TYPES.UNSIGNED_BYTE });
 worldData.wrapMode = PIXI.WRAP_MODES.CLAMP;
 worldData.mipmap = PIXI.MIPMAP_MODES.OFF;
@@ -55,7 +94,7 @@ async function createApp(): Promise<PIXI.Application<HTMLCanvasElement>> {
 
   await loadTextures();
 
-  const player = new Player(16, 24);
+  const player = new Player(2, 17);
 
   const renderer = new Renderer(world, app);
   renderer.updateTerrain();
@@ -64,14 +103,15 @@ async function createApp(): Promise<PIXI.Application<HTMLCanvasElement>> {
   const inputHandler = new InputHandler(app, game);
   inputHandler.updateHandlers(DEFAULT_INPUT_MAP);
 
-  const d = new Dynamite(14, 24);
-  d.data.vx = -0.01;
-  game.spawn(d);
+  // const d = new Dynamite(14, 24);
+  // d.data.vx = -0.01;
+  // game.spawn(d);
   game.spawn(player);
 
   const inventory: PlayerInventory = {
     selected: 0,
     slots: [
+      undefined,
       {
         id: 'dynamite',
         quantity: 5,

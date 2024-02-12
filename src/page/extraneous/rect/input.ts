478a1ac633988610c22ea5bb22ccec77e07e7866
jaroncui <jaron.cui@gmail.com>
2024-02-12 15:34:09 -0500

    rewiring input handler class to be more generic

--- src/page/extraneous/rect/input.ts
+++ src/page/extraneous/rect/input.ts
@@ -15,25 +15,36 @@ export const DEFAULT_INPUT_MAP: InputMap = {
   scrollDown: ['scrolldown', 'ArrowLeft']
 };
 
-type MouseClickInput = 'leftclick' | 'rightclick';
-type MouseScrollInput = 'scrollup' | 'scrolldown';
-type KeyChangeInput = {
+type InputListener<T> = {
+  type: 'leftclick' | 'rightclick';
+  handler: (clickEnded: boolean, inputState: InputStatee<T>) => void;
+} | {
+  type: 'scrollup' | 'scrolldown';
+  handler: (inputState: InputStatee<T>) => void;
+} | {
+  type: 'keyChange';
   key: string;
-  pressed: boolean;
-};
-type TypedInput = {
+  handler: (pressed: boolean, inputState: InputStatee<T>) => void;
+} | {
+  type: 'typed';
   key: string;
+  handler: (key: string, inputState: InputStatee<T>) => void;
 }
-type InputType = MouseClickInput | MouseScrollInput | KeyChangeInput | TypedInput;
-type InputListener = (
-  [MouseClickInput, (screenPosition: [number, number], inputState: InputState) => void] |
-  [MouseScrollInput, (inputState: InputState) => void] |
-  [KeyChangeInput, (pressed: boolean, inputState: InputState) => void] |
-  [TypedInput, (key: string, inputState: InputState) => void]
-);
 
-interface InputListenerSet {
-  listeners: InputListener[];
+type InputMapp<T> = {
+  [key in keyof T]: InputListener<T>;
+}
+
+type InputStatee<T> = {
+  buttons: {
+    [key in keyof T]: boolean;
+  };
+  cursorPosition: [number, number];
+}
+
+interface InputListenerSet<T> {
+  listeners: InputMapp<T>;
+  inputState: InputStatee<T>;
   enabled: boolean;
 }
 
@@ -82,103 +93,100 @@ export type InputTriggers = {
 export class InputHandler {
   app: PIXI.Application<HTMLCanvasElement>
   handlers: [any, string, (event: any) => void][];
-  triggers: InputTriggers;
-  inputState: InputState;
+  // triggers: InputTriggers;
+  inputListenerSets: InputListenerSet<any>[];
 
-  constructor(app: PIXI.Application<HTMLCanvasElement>, game: Game) {
+  constructor(app: PIXI.Application<HTMLCanvasElement>) {
     this.app = app;
     this.handlers = [];
-    this.inputState = {
-      leftClick: false,
-      rightClick: false,
-      mousePosition: [0, 0],
-      buttonsDown: {
-        useMain: false,
-        useSecondary: false,
-        up: false,
-        left: false,
-        down: false,
-        right: false,
-        jump: false,
-        control: false,
-        shift: false,
-        scrollUp: false,
-        scrollDown: false
-      }
-    }
-    this.triggers = game.getControlInterface(this.inputState);
+    this.inputListenerSets = [];
+    this.initializeHandlers();
+    // this.triggers = game.getControlInterface(this.inputState);
   }
 
-  updateHandlers(inputMap: InputMap) {
+  registerInputListeners(inputListenerSet: InputListenerSet<unknown>) {
+    this.inputListenerSets.push(inputListenerSet);
+  }
+
+  private initializeHandlers() {
     this.clearHandlers();
 
-    const leftClickTriggers: ButtonPressAction[] = [];
-    const rightClickTriggers: ButtonPressAction[] = [];
-    const scrollUpTriggers: ButtonPressAction[] = [];
-    const scrollDownTriggers: ButtonPressAction[] = [];
-    const keyboardTriggers: [string, ButtonPressAction][] = [];
+    // const leftClickTriggers: ButtonPressAction[] = [];
+    // const rightClickTriggers: ButtonPressAction[] = [];
+    // const scrollUpTriggers: ButtonPressAction[] = [];
+    // const scrollDownTriggers: ButtonPressAction[] = [];
+    // const keyboardTriggers: [string, ButtonPressAction][] = [];
 
-    const mouseButtonTriggers: Record<string, ButtonPressAction[]> = {
-      leftclick: leftClickTriggers,
-      rightclick: rightClickTriggers,
-      scrollup: scrollUpTriggers,
-      scrolldown: scrollDownTriggers
-    };
+    // const mouseButtonTriggers: Record<string, ButtonPressAction[]> = {
+    //   leftclick: leftClickTriggers,
+    //   rightclick: rightClickTriggers,
+    //   scrollup: scrollUpTriggers,
+    //   scrolldown: scrollDownTriggers
+    // };
 
-    let input: ButtonPressAction;
-    for (input in inputMap) {
-      const bindings = inputMap[input];
-      bindings.forEach(binding => {
-        const mouseButtonTrigger = mouseButtonTriggers[binding];
-        mouseButtonTrigger ? mouseButtonTrigger.push(input) : keyboardTriggers.push([binding, input]);
-      })
-    }
-    const mouseDown = () => leftClickTriggers.forEach(trigger => this.triggers.onButtonPress[trigger](true, this.inputState));
-    const mouseUp = () => leftClickTriggers.forEach(trigger => this.triggers.onButtonPress[trigger](false, this.inputState));
-    const rightDown = () => rightClickTriggers.forEach(trigger => this.triggers.onButtonPress[trigger](true, this.inputState));
-    const rightUp = () => rightClickTriggers.forEach(trigger => this.triggers.onButtonPress[trigger](false, this.inputState));
-    const keyDown = (event: KeyboardEvent) => {
-      keyboardTriggers.forEach(([key, trigger]) => {
-        if (event.key.toLowerCase() === key.toLowerCase()) {
-          this.inputState.buttonsDown[trigger] = true;
-          this.triggers.onButtonPress[trigger](true, this.inputState);
+    // let input: ButtonPressAction;
+    // for (input in inputMap) {
+    //   const bindings = inputMap[input];
+    //   bindings.forEach(binding => {
+    //     const mouseButtonTrigger = mouseButtonTriggers[binding];
+    //     mouseButtonTrigger ? mouseButtonTrigger.push(input) : keyboardTriggers.push([binding, input]);
+    //   })
+    // }
+    const clickHandler = (clickType: 'leftclick' | 'rightclick', down: boolean) => () => this.inputListenerSets.forEach(inputListenerSet => {
+      Object.entries(inputListenerSet.listeners).forEach(([binding, listener]) => {
+        if (listener.type === clickType) {
+          inputListenerSet.inputState.buttons[binding] = down;
+          inputListenerSet.enabled && listener.handler(!down, inputListenerSet.inputState);
         }
       });
-    }
-    const keyUp = (event: KeyboardEvent) => {
-      keyboardTriggers.forEach(([key, trigger]) => {
-        if (event.key.toLowerCase() === key.toLowerCase()) {
-          this.inputState.buttonsDown[trigger] = false;
-          this.triggers.onButtonPress[trigger](false, this.inputState);
-        }
+    });
+    const leftClick = clickHandler('leftclick', true);
+    const leftUnClick = clickHandler('leftclick', false);
+    const rightClick = clickHandler('rightclick', true);
+    const rightUnClick = clickHandler('rightclick', false);
+    const keyHandler = (down: boolean) => (event: KeyboardEvent) => {
+      this.inputListenerSets.forEach(inputListenerSet => {
+        Object.entries(inputListenerSet.listeners).forEach(([binding, listener]) => {
+          if (listener.type === 'keyChange' && listener.key.toLowerCase() === event.key.toLowerCase()) {
+            inputListenerSet.inputState.buttons[binding] = down;
+            inputListenerSet.enabled && listener.handler(down, inputListenerSet.inputState);
+          }
+          // TODO: probably move this into the proper typing handler...
+          if (!down && listener.type === 'typed') {
+            inputListenerSet.enabled && listener.handler(event.key, inputListenerSet.inputState);
+          }
+        });
       });
     }
+    const keyDown = keyHandler(true);
+    const keyUp = keyHandler(false);
+
     const scroll = (event: WheelEvent) => {
       const sign = Math.sign(event.deltaY);
-      const triggers = sign > 0 ? scrollUpTriggers : scrollDownTriggers;
-      triggers.forEach(trigger => {
-        this.triggers.onButtonPress[trigger](true, this.inputState);
-        this.triggers.onButtonPress[trigger](false, this.inputState);
-      });
+      this.inputListenerSets.forEach(inputListenerSet => {
+        Object.entries(inputListenerSet.listeners).forEach(([binding, listener]) => {
+          sign > 0 && listener.type === 'scrollup' && listener.handler(inputListenerSet.inputState);
+          sign < 0 && listener.type === 'scrolldown' && listener.handler(inputListenerSet.inputState);
+        })
+      })
     };
     const mouseMove = (event: MouseEvent) => {
-      this.inputState.mousePosition = [event.screenX, event.screenY];
-      this.triggers.onPointerMove([event.screenX, event.screenY]);
+      this.inputListenerSets.forEach(inputSet => inputSet.inputState.cursorPosition = [event.screenX, event.screenY])
     };
 
-    this.app.stage.addEventListener('mousedown', mouseDown);
-    this.app.stage.addEventListener('mouseup', mouseUp);
-    this.app.stage.addEventListener('rightdown', rightDown);
-    this.app.stage.addEventListener('rightup', rightUp);
+    this.app.stage.addEventListener('mousedown', leftClick);
+    this.app.stage.addEventListener('mouseup', leftUnClick);
+    this.app.stage.addEventListener('rightdown', rightClick);
+    this.app.stage.addEventListener('rightup', rightUnClick);
     this.app.stage.addEventListener('wheel', scroll);
     this.app.stage.addEventListener('mousemove', mouseMove);
     document.addEventListener('keydown', keyDown);
     document.addEventListener('keyup', keyUp);
 
-    this.handlers.push([this.app.stage, 'mousedown', mouseDown]);
-    this.handlers.push([this.app.stage, 'mouseup', mouseUp]);
-    this.handlers.push([this.app.stage, 'rightdown', rightDown]);
-    this.handlers.push([this.app.stage, 'rightup', rightUp]);
+    this.handlers.push([this.app.stage, 'mousedown', leftClick]);
+    this.handlers.push([this.app.stage, 'mouseup', leftUnClick]);
+    this.handlers.push([this.app.stage, 'rightdown', rightClick]);
+    this.handlers.push([this.app.stage, 'rightup', rightUnClick]);
     this.handlers.push([this.app.stage, 'wheel', scroll]);
     this.handlers.push([this.app.stage, 'mousemove', mouseMove]);
     this.handlers.push([document, 'keydown', keyDown]);

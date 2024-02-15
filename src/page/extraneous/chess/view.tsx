30873467523069182d68936602b7b1f62174c374
jaroncui <jaron.cui@gmail.com>
2024-02-15 14:33:12 -0500

    moved chess renderer into its own file

--- src/page/extraneous/chess/view.tsx
+++ src/page/extraneous/chess/view.tsx
@@ -1,121 +1,11 @@
 import * as PIXI from 'pixi.js';
 import { useEffect, useRef } from 'react';
-import { BoardLocation, BoardSpace, ChessBoard } from './chess';
-
-function frame(x: number, y: number, w: number, h: number) {
-  return {
-    frame: {x: x, y: y, w: w, h: h},
-    spriteSourceSize: {x: 0, y: 0, w: w, h: h},
-    sourceSize: {w: w, h: h}
-  }
-}
-
-const CHESS_PIECE_SCHEMA = {
-  frames: {
-    pawn: frame(0, 0, 15, 25),
-    rook: frame(0, 25, 15, 25),
-    knight: frame(0, 50, 15, 25),
-    bishop: frame(0, 75, 15, 25),
-    queen: frame(0, 100, 15, 25),
-    king: frame(0, 125, 15, 25),
-  }, meta: {
-    image: 'chesspieces2.png',
-    format: 'RGBA8888',
-    size: {w: 15, h: 25 * 6},
-    scale: '1'
-  }
-}
-
-const TEXTURES: Record<string, PIXI.Texture<PIXI.Resource>> = {};
-
-async function loadTextures() {
-  const blackPieceTextures = PIXI.BaseTexture.from(CHESS_PIECE_SCHEMA.meta.image);
-  blackPieceTextures.scaleMode = PIXI.SCALE_MODES.NEAREST;
-  const blackPieceSpritesheet = new PIXI.Spritesheet(blackPieceTextures, CHESS_PIECE_SCHEMA);
-  await blackPieceSpritesheet.parse();
-  Object.entries(blackPieceSpritesheet.textures).forEach(([piece, texture]) => {
-    TEXTURES['black' + piece] = texture;
-  });
-  const whitePieceTextures = PIXI.BaseTexture.from('chesspieceswhite.png');
-  whitePieceTextures.scaleMode = PIXI.SCALE_MODES.NEAREST;
-  const whitePieceSpritesheet = new PIXI.Spritesheet(whitePieceTextures, CHESS_PIECE_SCHEMA);
-  await whitePieceSpritesheet.parse();
-  Object.entries(whitePieceSpritesheet.textures).forEach(([piece, texture]) => {
-    TEXTURES['white' + piece] = texture;
-  });
-}
-
-function getTexture(name: string) {
-  return TEXTURES[name] || PIXI.Texture.EMPTY;
-}
-
-class Renderer {
-  board: ChessBoard;
-  app: PIXI.Application<HTMLCanvasElement>;
-  pieceSprites: Map<number, PIXI.Sprite>;
-  layers: PIXI.Container[];
-
-  constructor(app: PIXI.Application<HTMLCanvasElement>, board: ChessBoard) {
-    this.app = app;
-    this.board = board;
-    this.pieceSprites = new Map();
-
-    const boardTex = PIXI.BaseTexture.from('chessboard.png');
-    boardTex.scaleMode = PIXI.SCALE_MODES.NEAREST;
-    const boardSprite = PIXI.Sprite.from(boardTex);
-    boardSprite.scale.set(10);
-    app.stage.addChild(boardSprite);
-
-    this.layers = [0, 0, 0, 0, 0, 0, 0, 0].map(_ => new PIXI.Container());
-    this.app.stage.addChild(...this.layers);
-  }
-
-  rerender() {
-    // delete all piece sprites
-    this.pieceSprites.forEach(sprite => this.app.stage.removeChild(sprite));
-    // create piece sprites
-    this.pieceSprites = new Map();
-    this.board.board.forEach((row, y) => row.forEach((space, x) => {
-      if (space.piece) {
-        const sprite = PIXI.Sprite.from(getTexture(space.piece.color + (space.piece.name === 'freshPawn' ? 'pawn' : space.piece.name)));
-        sprite.anchor.set(0.5, 0.75);
-        sprite.scale.set(3.5);
-        sprite.x = (x + 0.5) * 50;
-        sprite.y = (y + 0.5) * 50;
-        const hash = y * 8 + x;
-        this.pieceSprites.set(hash, sprite);
-        this.layers[y].addChild(sprite);
-      }
-    }));
-  }
-
-  animateMovement(from: [number, number], to: [number, number]) {
-    const [fx, fy] = from;
-    const [tx, ty] = to;
-    const fromHash = fy * 8 + fx;
-    const toHash = ty * 8 + tx;
-    const sprite = this.pieceSprites.get(fromHash);
-    if (!sprite) {
-      return;
-    }
-    this.pieceSprites.delete(fromHash);
-    const pieceTaken = this.pieceSprites.get(toHash);
-    if (pieceTaken) {
-      this.layers[ty].removeChild(pieceTaken);
-    }
-
-    this.pieceSprites.set(toHash, sprite);
-    this.layers[fy].removeChild(sprite);
-    this.layers[ty].addChild(sprite);
-    sprite.x = (tx + 0.5) * 50;
-    sprite.y = (ty + 0.5) * 50;
-  }
-}
+import { BoardLocation, ChessBoard } from './chess';
+import { ChessBoardRenderer } from './render';
 
 async function createApp(): Promise<PIXI.Application<HTMLCanvasElement>> {
   const app = new PIXI.Application<HTMLCanvasElement>({ background: '#7acdeb', width: 600, height: 400 });
-  // app.renderer.addListener('mousepos', (event: MouseEvent) => console.log(event.clientX));
-  let t = 0;
+
   // Listen for animate update
   app.ticker.minFPS = 40;
   app.ticker.maxFPS = 40;
@@ -124,8 +14,8 @@ async function createApp(): Promise<PIXI.Application<HTMLCanvasElement>> {
   app.stage.hitArea = app.screen;
 
   const board = ChessBoard.setBoard();
-  await loadTextures();
-  const renderer = new Renderer(app, board);
+  await ChessBoardRenderer.loadTextures();
+  const renderer = new ChessBoardRenderer(app, board);
   let selected: BoardLocation | undefined;
 
   app.stage.addEventListener('click', event => {
@@ -141,7 +31,6 @@ async function createApp(): Promise<PIXI.Application<HTMLCanvasElement>> {
         ChessBoard.movePiece(board, selected, [x, y]);
         renderer.animateMovement(selected, [x, y]);
       } else {
-        
         // console.log('illegal')
       }
       selected = undefined;
@@ -153,16 +42,8 @@ async function createApp(): Promise<PIXI.Application<HTMLCanvasElement>> {
   })
 
   renderer.rerender();
-  // app.stage.addChild(pawnSprite);
 
-  app.ticker.add(delta => {
-    // board.board.forEach((row, y) => row.forEach((space, x) => {
-    //   if (space.piece) {
-    //     pawnSprite.x = (x + 0.5) * 50;
-    //     pawnSprite.y = (y + 0) * 50;
-    //   }
-    // }))
-  });
+  app.ticker.add(delta => { });
   return app;
 }
 
